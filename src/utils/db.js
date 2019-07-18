7569d9d01947e53c8117350d37398f951c63819a
kaihengann <annkaiheng@gmail.com>
2019-07-18 21:20:15 +0800

    Kai: Change db URI sequence

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const dbURI =
-  global.__MONGO_URI__ ||
   process.env.MONGODB_URI ||
-  'mongodb://localhost:27017/occupant';
+  global.__MONGO_URI__ ||
+  "mongodb://localhost:27017/occupant";
 console.log(global.__MONGO_URI__);
 
 mongoose.connect(dbURI, { useNewUrlParser: true });
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('connected to mongodb');
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("connected to mongodb");
 });
