5343c3324fd67fbf31e08aeae3c41f0060e8220e
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-07-18 21:00:12 +0800

    Revert to previous CORS whitelist

--- src/app.js
+++ src/app.js
@@ -7,29 +7,22 @@ const occupantRouter = require("./routes/occupant.route");
 const apartmentRouter = require("./routes/apartment.route");
 const cors = require("cors");
 
-const whitelist = [
-  "https://test-armadillo-arms.herokuapp.com/",
-  "https://test-armadillo-legs.herokuapp.com/",
-  "https://demo-armadillo-arms.herokuapp.com/",
-  "https://demo-armadillo-legs.herokuapp.com/",
-  "https://armadillo-arms.herokuapp.com/",
-  "https://armadillo-legs.herokuapp.com/",
-  "http://localhost:3000",
-  "http://localhost:3005"
-];
-
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  }
+  origin: [
+    "https://test-armadillo-arms.herokuapp.com/",
+    "https://armadillo-arms.herokuapp.com/",
+    "https://demo-armadillo-arms.herokuapp.com/",
+    "https://demo-armadillo-legs.herokuapp.com/",
+    "https://test-armadillo-legs.herokuapp.com/",
+    "https://armadillo-legs.herokuapp.com/",
+    "http://localhost:3000",
+    "http://localhost:3005"
+  ],
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 };
 
 app.use(cors(corsOptions));
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
