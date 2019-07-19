821576232871fd9a577d799725a7fbc7dc65bc22
kaihengann <annkaiheng@gmail.com>
2019-07-19 14:58:29 +0800

    Kai(instructed by GS): Testing if double quotes matter

--- src/app.js
+++ src/app.js
@@ -9,11 +9,11 @@ const cors = require("cors");
 
 const corsOptions = {
   origin: [
-    'https://test-armadillo-arms.herokuapp.com',
+    "https://test-armadillo-arms.herokuapp.com",
     'https://armadillo-arms.herokuapp.com',
-    'https://demo-armadillo-arms.herokuapp.com/',
-    'https://demo-armadillo-legs.herokuapp.com/',
-    'https://test-armadillo-legs.herokuapp.com',
+    "https://demo-armadillo-arms.herokuapp.com/",
+    "https://demo-armadillo-legs.herokuapp.com/",
+    "https://test-armadillo-legs.herokuapp.com",
     'https://armadillo-legs.herokuapp.com',
     'http://localhost:3000',
     'http://localhost:3005'
