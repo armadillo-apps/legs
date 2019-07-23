9921d1d55794c4c225b4b198035987833a09c745
kaihengann <annkaiheng@gmail.com>
2019-07-23 16:13:59 +0800

    Kai-Jon: Refactor occupant route and controller

--- src/controllers/occupant.controller.js
+++ src/controllers/occupant.controller.js
@@ -1,11 +1,27 @@
 const mongoose = require("mongoose");
 const OccupantModel = require("../models/occupant.model");
 
-module.exports.createOne = async person => {
-  const Occupant = new OccupantModel(person);
-  return await Occupant.save();
+const getOccupant = async (req, res, next) => {
+  try {
+    const foundOccupant = await OccupantModel.find();
+    res.status(200).send(foundOccupant);
+  } catch (err) {
+    const error = new Error("Unable to get occupant data");
+    return await res.status(400).json(error.message);
+  }
 };
 
-module.exports.getOccupant = async input => {
-  return await OccupantModel.findById(input);
+const createOccupant = async (req, res, next) => {
+  try {
+    const createdOccupant = await new OccupantModel(req.body);
+    await createdOccupant.save();
+    return res
+      .status(201)
+      .send(`Successfully added new occupant: ${createdOccupant.name}`);
+  } catch (err) {
+    const error = new Error("Unable to create new occupant");
+    return await res.status(400).json(error.message);
+  }
 };
+
+module.exports = { createOccupant, getOccupant };
--- src/routes/occupant.route.js
+++ src/routes/occupant.route.js
@@ -1,29 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const OccupantModel = require("../models/occupant.model");
-const OccupantController = require("../controllers/occupant.controller");
+const Ctrl = require("../controllers/occupant.controller");
 
-router.get("/", async (req, res, next) => {
-  try {
-    const foundOccupant = await OccupantModel.find();
-    res.status(200).send(foundOccupant);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.post("/", async (req, res, next) => {
-  let newOccupant = req.body;
-  try {
-
-    const createdOccupant = await OccupantController.createOne(newOccupant);
-    return res
-      .status(201)
-      .send(`Successfully added new occupant: ${createdOccupant.name}`);
-  } catch (err) {
-    const error = new Error("Unable to create new occupant");
-    return await res.status(400).json(error.message);
-  }
-});
+router.get("/", Ctrl.getOccupant);
+router.post("/", Ctrl.createOccupant);
 
 module.exports = router;
