763257fae0f191a8734644d8a09ebca3fa853e64
kaihengann <annkaiheng@gmail.com>
2019-07-23 16:01:06 +0800

    Kai-Jon: Refactor stay routes and controller

--- src/controllers/stay.controller.js
+++ src/controllers/stay.controller.js
@@ -1,14 +1,37 @@
-const mongoose = require("mongoose");
 require("../utils/db");
 const StayModel = require("../models/stay.model");
+const ApartmentModel = require("../models/apartment.model")
+const OccupantModel = require("../models/occupant.model")
 
-const getStayList = async (input) => {
-  return await StayModel.find({apartmentId: input});
-};
 
-const addStay = async input => {
-  const newStay = new StayModel(input);
-  return await newStay.save();
-};
+const getStayList = async (req, res, next) => {
+  try {
+    const foundStay = await StayModel.find({ apartmentId: req.params.apartmentId })
+    res.status(200).send(foundStay);
+  } catch (err) {
+    next(err);
+  }
+}
+
+const addStay = async (req, res, next) => {
+  try {
+    const foundApartment = await ApartmentModel.findById(req.body.apartmentId);
+    if (!foundApartment) {
+      throw new Error("Apartment not found");
+    }
+    const foundOccupant = await OccupantModel.findById(req.body.occupantId);
+    if (!foundOccupant) {
+      throw new Error("Occupant not found");
+    }
+    await new StayModel(req.body).save()
+    return res
+      .status(201)
+      .send(
+        `Successfully assigned ${foundOccupant.name} to ${foundApartment.name}`
+      );
+  } catch (err) {
+    return next(err);
+  }
+}
 
 module.exports = { getStayList, addStay };
--- src/routes/stay.route.js
+++ src/routes/stay.route.js
@@ -1,37 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { getStayList, addStay } = require("../controllers/stay.controller");
-const { getApartment } = require("../controllers/apartment.controller");
-const { getOccupant } = require("../controllers/occupant.controller");
 
-router.get("/apartments/:apartmentId", async (req, res, next) => {
-  try {
-    const foundStay = await getStayList(req.params.apartmentId);
-    res.status(200).send(foundStay);
-  } catch (err) {
-    next(err);
-  }
-});
+const Ctrl = require("../controllers/stay.controller")
 
-router.post("/", async (req, res, next) => {
-  try {
-    const foundApartment = await getApartment(req.body.apartmentId);
-    if (!foundApartment) {
-      throw new Error("Apartment not found");
-    }
-    const foundOccupant = await getOccupant(req.body.occupantId);
-    if (!foundOccupant) {
-      throw new Error("Occupant not found");
-    }
-    await addStay(req.body);
-    return res
-      .status(201)
-      .send(
-        `Successfully assigned ${foundOccupant.name} to ${foundApartment.name}`
-      );
-  } catch (err) {
-    return next(err);
-  }
-});
+router.get("/apartments/:apartmentId", Ctrl.getStayList);
+router.post("/", Ctrl.addStay);
 
 module.exports = router;
