9b6c7de1d8e871e56d53413bbb009aaba087052a
mtan014 <mtan014@e.ntu.edu.sg>
2019-10-31 11:20:17 +0800

    Melvin-LiShan: Added test for POST controller user login

--- __test__/users.test.js
+++ __test__/users.test.js
@@ -3,6 +3,7 @@ const request = require("supertest");
 const { mockUsers } = require("./mockData/mockData");
 
 describe("users CRUD tests", () => {
+
   describe("routes/users", () => {
     it("should return list of users", async () => {
       const userDbInstance = db.collection("users");
@@ -13,6 +14,17 @@ describe("users CRUD tests", () => {
       expect(response.status).toEqual(200);
       expect(Array.isArray(response.body)).toEqual(true);
       expect(response.body.length).toEqual(2);
+    });
+
+    it("should allow a valid user to log in", async () => {
+      const userDbInstance = db.collection("users");
+      await userDbInstance.insertMany(mockUsers);
+
+      const response = await request(app)
+        .post("/users/login")
+        .send({ email: "elson@thoughtworks.com", password: "pass1234" });
+
+      expect(response.status).toEqual(200);
     });
   });
 });
