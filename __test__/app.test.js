f492a7ca89ebd53f00ce014ca3abe2bdeb4c6dc5
squarepancakes <goh.shuyun@gmail.com>
2019-11-04 14:58:57 +0800

    Melvin-Yun #0: Fix linting on app.test.js

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,10 +1,10 @@
 const app = require("../src/app");
 const request = require("supertest");
 
-describe('Test app', () => {
-  it('should return hello', async () => {
+describe("Test app", () => {
+  it("should return hello", async () => {
     const response = await request(app).get("/");
 
-      expect(response.status).toEqual(200);
+    expect(response.status).toEqual(200);
   });
 });
