7bcd57107f12d31d961006f66ce89e9763230d2c
mtan014 <mtan014@e.ntu.edu.sg>
2019-11-04 12:12:00 +0800

    Melvin-Yun #0: Add test for app

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,6 +1,10 @@
+const app = require("../src/app");
+const request = require("supertest");
+
 describe('Test app', () => {
-  it('should return hello', () => {
-    
-    expect("Hello").toEqual("Hello");
+  it('should return hello', async () => {
+    const response = await request(app).get("/");
+
+      expect(response.status).toEqual(200);
   });
 });
