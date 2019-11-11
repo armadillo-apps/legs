de08de7c2447a44ef08937f393a7776028d63017
ShafoYoisho <muhammadsyafizuraimi@Gmail.com>
2019-11-11 11:48:57 +0800

    Carl-Syafi #12: Add a name field on user schema

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
+  name: { type: String },
   email: { type: String, required: true, index: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["admin", "manager"] }
