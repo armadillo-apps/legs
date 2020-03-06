dc6a315d575899ce628c7dda97bafee399bad6d8
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-03-06 17:03:52 +0800

    [#103] feature: add guest role

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String },
   email: { type: String, required: true, index: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, required: true, enum: ["admin", "manager"] }
+  role: { type: String, required: true, enum: ["admin", "manager", "guest"] }
 });
 
 userSchema.pre("save", async function(next) {
